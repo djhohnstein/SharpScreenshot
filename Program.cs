bf33ea5a37727bacf203cc7ff8a3b4be81057cc7
Dwight Hohnstein <dhohnstein@specterops.io>
2018-12-18 11:45:27 -0800

    First commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace SharpScreenshot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName;
+            if (args.Length == 1)
+            {
+                fileName = args[0];
+            }
+            else
+            {
+                fileName = System.IO.Path.GetTempPath() + Guid.NewGuid().ToString() + ".jpeg";
+            }
+            Rectangle bounds = Screen.GetBounds(Point.Empty);
+            try
+            {
+                using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height))
+                {
+                    using (Graphics g = Graphics.FromImage(bitmap))
+                    {
+                        g.CopyFromScreen(Point.Empty, Point.Empty, bounds.Size);
+                    }
+                    bitmap.Save(fileName, ImageFormat.Jpeg);
+                }
+                Console.WriteLine("[+] Saved screenshot to:");
+                Console.WriteLine("\t{0}", fileName);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("[X] Error: {0}", ex);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+# SharpScreenshot
+
+## Description
+
+Screenshot. In C#!
+
+## Usage
+
+Without any arguments, the default save location will be in the temp folder. Otherwise, save to the path in the first argument given.
+
+`.\SharpScreenshot.exe`
+
+`.\SharpScreenshot.exe C:\Users\USER\Desktop\temp.jpeg`
+
+## Example
+
+```
+C:\SharpScreenshot\bin\Debug>.\SharpScreenshot.exe
+Saved screenshot to:
+	C:\Users\USER\AppData\Local\Temp\a72fd144-1ded-472d-8558-d3eb80d3c924.jpeg
+```
